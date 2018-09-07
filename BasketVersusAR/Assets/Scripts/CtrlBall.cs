a853dadd5224f076b52b4d3d7b4db6a6cb254b26
Carlos <carlos.2380@hotmail.com>
2018-09-07 03:23:28 +0200

    Auto respawn of ball when fall or by time

--- BasketVersusAR/Assets/Scripts/CtrlBall.cs
+++ BasketVersusAR/Assets/Scripts/CtrlBall.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class CtrlBall : MonoBehaviour
 {
     private Rigidbody rigidBody;
+    private Vector3 initPosition = new Vector3();
+    private const float timeToInit = 5f;
+    private float timer = timeToInit;
 
 	// Use this for initialization
 	void Start ()
 	{
 	    rigidBody = GetComponent<Rigidbody>();
-	    rigidBody.AddForce(0f, 0, 80f);
-
-    }
+	    initPosition = transform.position;
+        rigidBody.AddForce(0f, 0, 80f);
+	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+	    timer -= Time.deltaTime;
+
+	    if (transform.position.y < -3f || timer < 0f)
+	    {
+	        timer = timeToInit;
+	        transform.position = initPosition;
+	        rigidBody.velocity = Vector3.zero;
+            rigidBody.AddForce(0f, 0, 80f);
+        }
 	}
 }
