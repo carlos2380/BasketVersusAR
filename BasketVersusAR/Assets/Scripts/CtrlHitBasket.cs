60e9c37d95001f50faf840e7f4a5bb6879ba318e
Carlos <carlos.2380@hotmail.com>
2018-09-07 18:40:48 +0200

    Hit in Basket whith triggers.

---
+++ BasketVersusAR/Assets/Scripts/CtrlHitBasket.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlHitBasket : MonoBehaviour
+{
+    public GameObject overTriggerGameObj;
+    public GameObject belowTriggerGameObj;
+    private TriggerBasket overTriggerBasket;
+    private TriggerBasket belowTriggerBasket;
+    private bool overTriggerActive = false;
+    private bool belowTriggerActive = false;
+
+    private void Start()
+    {
+        overTriggerBasket = overTriggerGameObj.GetComponent<TriggerBasket>();
+        belowTriggerBasket = belowTriggerGameObj.GetComponent<TriggerBasket>();
+    }
+
+    public void triggerChanged()
+    {
+        if (overTriggerActive == true && belowTriggerActive == false)
+        {
+            if (belowTriggerBasket.triggerActive == true)
+            {
+                Debug.Log("BASKET!!!");
+            }
+        }
+        overTriggerActive = overTriggerBasket.triggerActive;
+        belowTriggerActive = belowTriggerBasket.triggerActive;
+    }
+    
+    
+    public void restartTriggers()
+    {
+        overTriggerActive = false;
+        belowTriggerActive = false;
+    }
+    
+}
---
+++ BasketVersusAR/Assets/Scripts/TriggerBasket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBasket : MonoBehaviour
+{
+    [HideInInspector]
+    public bool triggerActive = false;
+    private CtrlHitBasket ctrlHitBasket;
+
+    private void Start()
+    {
+        ctrlHitBasket = transform.parent.parent.GetComponent<CtrlHitBasket>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            triggerActive = true;
+            ctrlHitBasket.triggerChanged();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerActive = false;
+        ctrlHitBasket.triggerChanged();
+    }
+}
