f85b51cbc41f48199bb2525847eaab5a985fc6d5
Carlos <carlos.2380@hotmail.com>
2018-09-07 02:58:09 +0200

    Bouncy ball from camera to baskets

---
+++ BasketVersusAR/Assets/Scripts/CtrlBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlBall : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    rigidBody = GetComponent<Rigidbody>();
+	    rigidBody.AddForce(0f, 0, 80f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
