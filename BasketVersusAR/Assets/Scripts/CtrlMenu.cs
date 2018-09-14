b8408927941aa7ff774d26005da168152c9ed4dd
Carlos <carlos.2380@hotmail.com>
2018-09-14 02:24:12 +0200

    Tutorial(Download)

--- BasketVersusAR/Assets/Scripts/CtrlMenu.cs
+++ BasketVersusAR/Assets/Scripts/CtrlMenu.cs
@@ -8,7 +8,11 @@ public class CtrlMenu : MonoBehaviour
     public GameObject basket;
     public GameObject panelMenu;
     public GameObject panelAbout;
-    
+    public GameObject panelTutorial;
+    public GameObject panelDownload;
+    public GameObject panelSelect;
+    public GameObject panelEnjoy;
+
     [Header("Sounds")]
     public AudioSource musicMenu;
     public AudioSource click;
@@ -19,6 +23,7 @@ public class CtrlMenu : MonoBehaviour
         basket.SetActive(true);
         panelMenu.SetActive(true);
         panelAbout.SetActive(false);
+        panelTutorial.SetActive(false);
     }
 	
     public void startGame()
@@ -30,6 +35,7 @@ public class CtrlMenu : MonoBehaviour
     public void home()
     {
         click.Play();
+        panelTutorial.SetActive(false);
         basket.SetActive(true);
         panelMenu.SetActive(true);
         panelAbout.SetActive(false);
@@ -59,6 +65,48 @@ public class CtrlMenu : MonoBehaviour
     {
         click.Play();
         Application.OpenURL("https://www.linkedin.com/in/carlos2380/");
+    }
+
+    public void tutorial()
+    {
+        click.Play();
+        basket.SetActive(false);
+        panelMenu.SetActive(false);
+        panelAbout.SetActive(false);
+        panelTutorial.SetActive(true);
+        panelDownload.SetActive(true);
+        panelSelect.SetActive(false);
+        panelEnjoy.SetActive(false);
+    }
+    
+    public void downloadPanel()
+    {
+        click.Play();
+        panelDownload.SetActive(true);
+        panelSelect.SetActive(false);
+        panelEnjoy.SetActive(false);
+    }
+
+    public void downloadImage()
+    {
+        click.Play();
+        Application.OpenURL("https://www.yahoo.com/");
+    }
+
+    public void selectPanel()
+    {
+        click.Play();
+        panelDownload.SetActive(false);
+        panelSelect.SetActive(true);
+        panelEnjoy.SetActive(false);
+    }
+
+    public void enjoyPanel()
+    {
+        click.Play();
+        panelDownload.SetActive(false);
+        panelSelect.SetActive(false);
+        panelEnjoy.SetActive(true);
     }
 
 }
